16eceae611e77ba2d7e0eb9dd4a263b2ded21097
hoto17296 <mail@hoto.me>
2017-11-02 17:49:30 +0900

    signin form

---
+++ src/Signin.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="ui container" style="max-width: 800px">
+    <form class="ui large form">
+      <h2>Sign In</h2>
+      <div class="field">
+        <div class="ui left icon input">
+          <i class="user icon"></i>
+          <input type="text" name="email" placeholder="E-mail address">
+        </div>
+      </div>
+      <div class="field">
+        <div class="ui left icon input">
+          <i class="lock icon"></i>
+          <input type="password" name="password" placeholder="Password">
+        </div>
+      </div>
+      <div class="ui large primary button">Sign In</div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'signin',
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Signin from './Signin.vue'
+
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+  router: new VueRouter({
+    mode: 'history',
+    routes: [
+      { path: '/', component: Signin }
+    ]
+  }),
+});
